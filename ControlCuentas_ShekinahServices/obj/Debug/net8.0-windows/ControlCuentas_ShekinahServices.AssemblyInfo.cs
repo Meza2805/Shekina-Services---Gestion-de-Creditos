b0d37639e6b068db87398c385096624bcf318423
Marvin Meza <101306102+Meza2805@users.noreply.github.com>
2025-04-11 16:20:25 -0600

    Avances

--- ControlCuentas_ShekinahServices/obj/Debug/net8.0-windows/ControlCuentas_ShekinahServices.AssemblyInfo.cs
+++ ControlCuentas_ShekinahServices/obj/Debug/net8.0-windows/ControlCuentas_ShekinahServices.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControlCuentas_ShekinahServices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76951bf3b899ba0a45d0cbbc55ca090ab6af4b9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d10a2d78055698fd9ef55b1be01319ad574ba3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControlCuentas_ShekinahServices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControlCuentas_ShekinahServices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
